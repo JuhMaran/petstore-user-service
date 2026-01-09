7de429226035affefdf82d4cc9fa1c65004dddac
JuhMaran <julianemaran@gmail.com>
2026-01-09 20:38:14 -0300

    doc: retira o campo 'password' do swagger

---
+++ user-service/src/main/java/com/petstore/user/model/User.java
@@ -0,0 +1,12 @@
+package com.petstore.user.model;
+
+/**
+ * user-service
+ * @author Juliane Maran
+ * @since 09/01/2026
+ */
+public class User {
+
+    // TODO: Implementar funcionalidades da classe User
+
+}
